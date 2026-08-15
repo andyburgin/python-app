65162b0e364deb0fd7615bedac8d463612ca6ccd
Andy Burgin <github@andyburgin.co.uk>
2026-08-15 13:05:34 +0100

    workflow short commit fix

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'v6'
+        'message': 'v7'
     })
 
 @app.route('/api/v1/healthz')

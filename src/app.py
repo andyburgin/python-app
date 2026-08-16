151483bfc3a1917e79fbf5b2796568f23132600c
Andy Burgin <github@andyburgin.co.uk>
2026-08-16 15:59:13 +0100

    update details to info

--- src/app.py
+++ src/app.py
@@ -4,13 +4,13 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
-def details():
+def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'v12'
+        'message': 'v13'
     })
 
 @app.route('/api/v1/healthz')
